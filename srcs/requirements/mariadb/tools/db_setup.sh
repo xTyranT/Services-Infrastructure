2527f4e78964a7ca6bb0cd8b8703985a01272703
xTyranT <oufer.kamal@gmail.com>
2023-12-10 17:08:24 +0100

    mariadb is working properly

---
+++ srcs/requirements/mariadb/tools/db_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+service mariadb start
+
+sleep 1
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$USER_PWD';"
+mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$USER_PWD';"
+mariadb -e "FLUSH PRIVILEGES;"
+
+service mariadb stop
+
+mysqld_safe
